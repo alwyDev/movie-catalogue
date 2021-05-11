638776e7830d9940fcaf308558de73469643ac9b
Alwy <wyalwy.dev@gmail.com>
2021-05-11 10:33:34 +0700

    first commit

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  KEY: 'ff6d558d936c8d6b5a7c7381873f9be7',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us'
+}
+
+export default CONFIG
